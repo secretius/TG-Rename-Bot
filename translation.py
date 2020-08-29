b76ef3cd2f6156b7fcd167fb89b6a15d05065ae3
secretius <70433831+secretius@users.noreply.github.com>
2020-08-29 16:07:33 +0500

    translation.py
    Yaxshimi?

--- translation.py
+++ translation.py
@@ -27,12 +27,12 @@ This is a Telegram Rename Bot!
 1. Send me any Telegram File.
 2. Reply to that message to /rename new name.extension.
    
-<b>👉 Create own Clone Bot :</b> 👉 <a href="https://youtu.be/yYHrSnuYXpA">Diploy</a>
+<b>👉 My channel is :</b> 👉 <a href="https://t.me/Secretius">Secretius</a>
 
 --------
 
 Support Group : @SecretiusGroup
-© @SpEcHlDe @TGBotsZ"""
+© @Secretius @Powered_By_Secretius"""
     REPLY_TO_DOC_FOR_RENAME_FILE = "Reply to a Telegram media to `/rename New Name.extension` with custom thumbnail support.."
     ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction."
     FREE_USER_LIMIT_Q_SZE = """Cannot Process.
